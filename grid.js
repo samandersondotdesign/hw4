2fe0ec2dbd2d053946e9e5e57b0b758a16b11fbd
samandersondotdesign <samuelanderson@me.com>
2017-02-15 08:19:01 -0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,15 +1,16 @@
-function setup() { 
-  createCanvas(400, 400);
-} 
+function setup() {
+	createCanvas(400, 400);
+}
 
-function draw() { 
-  background(220);
+function draw() {
+
+	noStroke();
+	fill(255);
+	background(0);
 	
-	for(var x=0; x <= width; x+=30){
-		for (var y=0; y <= height; y+=30){
-			ellipse(x,y,20,20);
+	for (var x = 20; x <= width; x += 30) {
+		for (var y = 20; y <= height; y += 30) {
+			ellipse(x, y, 20, 20);
 		}
 	}
-	
-	
 }
